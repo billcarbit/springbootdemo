bcf4a502cd986996fdd5989b0c54305240e29dae
billjoy <517542596@qq.com>
2018-11-09 00:45:16 +0800

    dsdsds

--- demo/src/main/java/com/wangning/example/demo/interceptor/MyInterceptor1.java
+++ demo/src/main/java/com/wangning/example/demo/interceptor/MyInterceptor1.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Component
 public class MyInterceptor1 implements HandlerInterceptor {
@@ -14,7 +15,28 @@ public class MyInterceptor1 implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+      /*  String principal =
+                request.getUserPrincipal().getName();*/
+        request.getSession(true);//创建session,HttpOnly
+        request.getSession().invalidate();//删除session
+        String method = request.getMethod();
+        String requestURI = request.getRequestURI();
+        String servletPath = request.getServletPath();
+        String remoteUser = request.getRemoteUser();
+        String remoteAddr = request.getRemoteAddr();
+        String protocol = request.getProtocol();
+        String sessionId = request.getRequestedSessionId();
+        String serverName = request.getServerName();
         System.out.println("preHandle1");
+      //  System.out.println("principal="+principal);
+        System.out.println("method="+method);
+        System.out.println("requestURI="+requestURI);
+        System.out.println("servletPath="+servletPath);
+        System.out.println("remoteUser="+remoteUser);
+        System.out.println("remoteAddr="+remoteAddr);
+        System.out.println("protocol="+protocol);
+        System.out.println("sessionId="+sessionId);
+        System.out.println("serverName="+serverName);
         return true;
     }
 
